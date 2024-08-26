214b79fb6627789d3d305990891d902c7cf3727e
kushalchh07 <kushalchhetri54@gmail.com>
2024-08-26 23:20:26 +0545

    feat: get method create and task model create

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Task
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'isDone', 'date')
+    list_filter = ('isDone', 'date')
+    search_fields = ('title', 'description')
 # Register your models here.
--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+# List all tasks or create a new one
+class TaskListCreateView(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+# Retrieve, update, or delete a specific task
+class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
